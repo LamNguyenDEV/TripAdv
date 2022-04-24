993979fd73671e2158f969b6538537aa9effa78f
LamNguyenDEV <lamgolmart@gmail.com>
2022-04-24 18:39:35 -0400

    adding 6 destination pages

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express= require ("express");
 const bodyParser = require ("body-parser");
 const ejs = require("ejs");
 
+const path= require('path');
+
 const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require ("passport");
@@ -11,13 +13,14 @@ const LocalStrategy = require('passport-local');
 const passportLocalMongoose = require('passport-local-mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const findOrCreate = require("mongoose-findorcreate");
+const req = require('express/lib/request');
 
 const app = express();
 
 // app.use(express.static("./public"));
 app.use(express.static("assets"));
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs','html');
 
 app.use(bodyParser.urlencoded({
     extended: true 
@@ -39,6 +42,7 @@ mongoose.connect ("mongodb://localhost:27017/userTrip", {useNewUrlParser: true})
 const userSchema =  new mongoose.Schema ({
     email: String ,
     password: String,
+    name: String,
     // googleId: String,
     secret: String
 }); 
@@ -58,6 +62,8 @@ passport.deserializeUser(User.deserializeUser());
 
 //------------------------------------------------>>> Server body function<<<--------------------------------------
 app.get("/", function(req, res) {
+    //demo
+    // res.sendFile(path.join(__dirname+'/index.html'));
     res.render("home");
 });
 
@@ -69,17 +75,24 @@ app.get("/register", function (req, res) {
 })
 //handle secret pages
 app.get("/homepage", function (req, res) {
+
+
+    if( req.isAuthenticated()) {
+        res.render("homepage");
+    }else {
+        res.redirect("login");
+    }
     // looking in our database for all secret property not equal to null
-     User.find({"secret": {$ne: null}},function (err, foundUsers){
-         if(err){
-             console.log(err);
+    //  User.find({"secret": {$ne: null}},function (err, foundUsers){
+    //      if(err){
+    //          console.log(err);
     
-         }else { 
-             if(foundUsers) {
-                 res.render("homepage", {usersWithSecrets: foundUsers});
-             }
-         }
-     });
+    //      }else { 
+    //          if(foundUsers) {
+    //              res.render("homepage", {usersWithSecrets: foundUsers});
+    //          }
+    //      }
+    //  });
     });
 
 //handle register pages
@@ -100,7 +113,7 @@ app.post ("/register", function (req, res) {
     });
 
 // after login --> authenticate--> move to /secrets page
-    app.post ("/login", function (req,res) {
+app.post ("/login", function (req,res) {
     
         const user = new User( {
             username : req.body.username,
@@ -114,12 +127,72 @@ app.post ("/register", function (req, res) {
                 console.log(err);
             } else {
                 passport.authenticate("local")(req, res, function () {
+                    
                     res.redirect("/homepage");
                 });
             }
         });
      
     }) ;
+
+// handle logout 
+app.get("/logout", (req,res)=>{
+    req.logout();
+    res.redirect("/");
+})
+// --->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>handle each section
+// handle each destination
+app.
+get("/newyork", function (req, res) {
+
+    if( req.isAuthenticated()) {
+        res.render("newyork");
+    }else {
+        res.redirect("login");
+    }
+   
+})
+
+.get("/tokyo", function (req, res) {
+
+    if( req.isAuthenticated()) {
+        res.render("tokyo");
+    }else {
+        res.redirect("login");
+    }  
+})
+.get("/sydney", function (req, res) {
+
+    if( req.isAuthenticated()) {
+        res.render("sydney");
+    }else {
+        res.redirect("login");
+    }  
+})
+.get("/cairo", function (req, res) {
+
+    if( req.isAuthenticated()) {
+        res.render("cairo");
+    }else {
+        res.redirect("login");
+    }  
+})
+.get("/rome", function (req, res) {
+
+    if( req.isAuthenticated()) {
+        res.render("rome");
+    }else {
+        res.redirect("login");
+    }  
+})
+.get("/paris", function (req, res) {
+
+    if( req.isAuthenticated()) {
+        res.render("paris");
+    }else {
+        res.redirect("login");
+    }  
+})
 
 
 
