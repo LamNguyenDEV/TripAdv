19d705db03d9448e5da7db3dffa7ee733188673e
LamNguyenDEV <lamgolmart@gmail.com>
2022-05-12 23:07:56 -0400

    adding heroku sever

--- app.js
+++ app.js
@@ -15,7 +15,14 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const findOrCreate = require("mongoose-findorcreate");
 const req = require('express/lib/request');
 
+
+// const popup = require("node-popup");
+// var popup = require("popups");
+
+var alert = require('alert');
+
 const app = express();
+
 
 // app.use(express.static("./public"));
 app.use(express.static("assets"));
@@ -37,8 +44,9 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 // connect local database
-mongoose.connect ("mongodb://localhost:27017/userTrip", {useNewUrlParser: true});
+// mongoose.connect ("mongodb://localhost:27017/userTrip", {useNewUrlParser: true});
 //connect remote database
+mongoose.connect("mongodb+srv://admin-lam:Test123@cluster0.velxy.mongodb.net/tripUser",{useNewUrlParser: true});
 // mongoose.connect("mongodb+srv://caracciod1:<password>@cluster0.mz6vx.mongodb.net/test,{useNewUrlParser: true}); 
 // Schema for user model
 const userSchema =  new mongoose.Schema ({
@@ -86,10 +94,13 @@ passport.deserializeUser(User.deserializeUser());
 app.get("/", function(req, res) {
     //demo
     // res.sendFile(path.join(__dirname+'/index.html'));
+   
     res.render("home");
 });
 
 app.get("/login", function (req, res) {
+   
+  
     res.render("login");
 })
 app.get("/register", function (req, res) {
@@ -120,18 +131,20 @@ app.get("/homepage", function (req, res) {
 //handle register pages
 app.post ("/register", function (req, res) {
     // console.log(req.body.fullname);
-
-   
+      
         User.register({username: req.body.username}, req.body.password , function(err, user){
             if(err) {
                 console.log(err);
+                   
+                alert("****Unsucessful Register.Please try again****");
                     res.redirect("/register");
-    
             }else {
                 passport.authenticate("local") (req,res, function(){
 
                     User.findById(req.user.id, function (err,foundUser) {
-                        if(!err) {foundUser.name=req.body.fullname ;
+                        if(!err) {
+                        foundUser.name=req.body.fullname ;
+                        alert("****Sucessful Register.Welcome new member!!****");
                         foundUser.save();
                         }
 
@@ -156,7 +169,11 @@ app.post ("/login", function (req,res) {
     // Read Document https://www.passportjs.org/concepts/authentication/login/
         req.login(user, function (err){
             if(err) {
-                console.log(err);
+                alert("Login is failed. Please try again");
+                // console.log(err);
+                console.log("this go here");
+                res.redirect("/login");
+                
             } else {
                 passport.authenticate("local")(req, res, function () {
                     
@@ -171,7 +188,7 @@ app.post ("/login", function (req,res) {
 app.get("/logout", (req,res)=>{
     req.logout();
     res.redirect("/");
-})
+});
 // --->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>handle each section
 // handle each destination
 app.
@@ -198,53 +215,128 @@ app.
         }
     
     })
-
-    .get("/tokyo", function (req, res) {
-
-        
-
-        if( req.isAuthenticated()) {
-            
-            res.render("tokyo");
-        }else {
-            res.redirect("login");
-        }  
-    })
-    .get("/sydney", function (req, res) {
-
-        if( req.isAuthenticated()) {
-            res.render("sydney");
-        }else {
-            res.redirect("login");
-        }  
-    })
+//cairo
     .get("/cairo", function (req, res) {
+        console.log(req.user.username); 
+        if( req.isAuthenticated()) { 
+            
+            Place.find({ namePlace:"cairo"}, function (err,docs){
+                if(err) {
+                    console.log(err);
+                    res.render("cairo",{cairoReview:res.locals.cairoReview});
 
-        if( req.isAuthenticated()) {
-            console.log("hello Cairo");
-            res.render("cairo");
-           
+                } else {
+                    
+                    res.render("cairo",{cairoReview:docs,user:req.user});
+                    
+                } // use res.locals for data in render template
+             });
+            
+            
+            
         }else {
             res.redirect("login");
-        }  
+        }
+    
     })
+    // paris
+   .get("/paris", function (req, res) {
+        console.log(req.user.username); 
+        if( req.isAuthenticated()) { 
+            
+            Place.find({ namePlace:"paris"}, function (err,docs){
+                if(err) {
+                    console.log(err);
+                    res.render("paris",{parisReview:res.locals.cairoReview});
+
+                } else {
+                    
+                    res.render("paris",{parisReview:docs,user:req.user});
+                    
+                } // use res.locals for data in render template
+             });
+            
+            
+            
+        }else {
+            res.redirect("login");
+        }
+    
+    })
+    //rome
     .get("/rome", function (req, res) {
+        console.log(req.user.username); 
+        if( req.isAuthenticated()) { 
+            
+            Place.find({ namePlace:"rome"}, function (err,docs){
+                if(err) {
+                    console.log(err);
+                    res.render("rome",{romeReview:res.locals.cairoReview});
 
-        if( req.isAuthenticated()) {
-            res.render("rome");
+                } else {
+                    
+                    res.render("rome",{romeReview:docs,user:req.user});
+                    
+                } // use res.locals for data in render template
+             });
+            
+            
+            
         }else {
             res.redirect("login");
-        }  
+        }
+    
     })
-    .get("/paris", function (req, res) {
+    //sydney
+    .get("/sydney", function (req, res) {
+        console.log(req.user.username); 
+        if( req.isAuthenticated()) { 
+            
+            Place.find({ namePlace:"sydney"}, function (err,docs){
+                if(err) {
+                    console.log(err);
+                    res.render("sydney",{sydneyReview:res.locals.cairoReview});
 
-        if( req.isAuthenticated()) {
-            res.render("paris");
+                } else {
+                    
+                    res.render("sydney",{sydneyReview:docs,user:req.user});
+                    
+                } // use res.locals for data in render template
+             });
+            
+            
+            
         }else {
             res.redirect("login");
-        }  
-    });
-// handle post each destination
+        }
+    
+    })
+    //tokyo
+    .get("/tokyo", function (req, res) {
+        console.log(req.user.username); 
+        if( req.isAuthenticated()) { 
+            
+            Place.find({ namePlace:"tokyo"}, function (err,docs){
+                if(err) {
+                    console.log(err);
+                    res.render("tokyo",{tokyoReview:res.locals.cairoReview});
+
+                } else {
+                    
+                    res.render("tokyo",{tokyoReview:docs,user:req.user});
+                    
+                } // use res.locals for data in render template
+             });
+            
+            
+            
+        }else {
+            res.redirect("login");
+        }
+    
+    })
+// ---------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>handle post each destination
+//newyork
 app.post("/newyork", function(req,res){
     console.log("post /newyork");
     const submittedSecret= req.body.secret;
@@ -267,10 +359,157 @@ app.post("/newyork", function(req,res){
     if (err) {return console.log(err);
     } else { 
         small.save();
+        alert("***Sucessful post your post***");
         res.redirect("newyork")}
 
 });
-})
+});
+
+
+////Paris
+app.post("/paris", function(req,res){
+    console.log("post /paris");
+    const submittedSecret= req.body.secret;
+    const nameReview = req.body.name;  
+    
+    Place.create({namePlace: "paris",
+                    review: submittedSecret,
+                    userid: req.body.userfullName,
+                    time: new Date(),
+                    rateHeart:req.body.rateHeart, 
+                    productPackages:req.body.productPackages,
+                    cultural: req.body.cultural,
+                    gastronomy:req.body.gastronomy,
+                    shopping: req.body.shopping,
+                    infrastructureTransport:req.body.Infras_transport,
+                    landscapeNaturalResource:req.body.landscape,
+                    userGmail: req.body.userEmail,                 
+
+},function (err,small){
+    if (err) {return console.log(err);
+    } else { 
+        alert("***Sucessful post your post***");
+        small.save();
+        res.redirect("paris")}
+
+});
+});
+
+//rome
+app.post("/cairo", function(req,res){
+    console.log("post /rome");
+    const submittedSecret= req.body.secret;
+    const nameReview = req.body.name;  
+    
+    Place.create({namePlace: "cairo",
+                    review: submittedSecret,
+                    userid: req.body.userfullName,
+                    time: new Date(),
+                    rateHeart:req.body.rateHeart, 
+                    productPackages:req.body.productPackages,
+                    cultural: req.body.cultural,
+                    gastronomy:req.body.gastronomy,
+                    shopping: req.body.shopping,
+                    infrastructureTransport:req.body.Infras_transport,
+                    landscapeNaturalResource:req.body.landscape,
+                    userGmail: req.body.userEmail,                 
+
+},function (err,small){
+    if (err) {return console.log(err);
+    } else { 
+        small.save();
+        alert("***Sucessful post your post***");
+        res.redirect("cairo")}
+
+});
+});
+
+//sydney
+app.post("/sydney", function(req,res){
+    console.log("post /sydney");
+    const submittedSecret= req.body.secret;
+    const nameReview = req.body.name;  
+    
+    Place.create({namePlace: "sydney",
+                    review: submittedSecret,
+                    userid: req.body.userfullName,
+                    time: new Date(),
+                    rateHeart:req.body.rateHeart, 
+                    productPackages:req.body.productPackages,
+                    cultural: req.body.cultural,
+                    gastronomy:req.body.gastronomy,
+                    shopping: req.body.shopping,
+                    infrastructureTransport:req.body.Infras_transport,
+                    landscapeNaturalResource:req.body.landscape,
+                    userGmail: req.body.userEmail,                 
+
+},function (err,small){
+    if (err) {return console.log(err);
+    } else { 
+        small.save();
+        alert("***Sucessful post your post***");
+        res.redirect("sydney")}
+
+});
+});
+//Rome
+app.post("/rome", function(req,res){
+    console.log("post/rome");
+    const submittedSecret= req.body.secret;
+    const nameReview = req.body.name;  
+    
+    Place.create({namePlace: "rome",
+                    review: submittedSecret,
+                    userid: req.body.userfullName,
+                    time: new Date(),
+                    rateHeart:req.body.rateHeart, 
+                    productPackages:req.body.productPackages,
+                    cultural: req.body.cultural,
+                    gastronomy:req.body.gastronomy,
+                    shopping: req.body.shopping,
+                    infrastructureTransport:req.body.Infras_transport,
+                    landscapeNaturalResource:req.body.landscape,
+                    userGmail: req.body.userEmail,                 
+
+},function (err,small){
+    if (err) {return console.log(err);
+    } else { 
+        small.save();
+        alert("***Sucessful post your post***");
+        res.redirect("rome")}
+
+});
+});
+
+//tokyo
+app.post("/tokyo", function(req,res){
+    console.log("post /tokyo");
+    const submittedSecret= req.body.secret;
+    const nameReview = req.body.name;  
+    
+    Place.create({namePlace: "tokyo",
+                    review: submittedSecret,
+                    userid: req.body.userfullName,
+                    time: new Date(),
+                    rateHeart:req.body.rateHeart, 
+                    productPackages:req.body.productPackages,
+                    cultural: req.body.cultural,
+                    gastronomy:req.body.gastronomy,
+                    shopping: req.body.shopping,
+                    infrastructureTransport:req.body.Infras_transport,
+                    landscapeNaturalResource:req.body.landscape,
+                    userGmail: req.body.userEmail,                 
+
+},function (err,small){
+    if (err) {return console.log(err);
+    } else { 
+        small.save();
+        alert("***Sucessful post your post***");
+        res.redirect("tokyo")}
+
+});
+});
+
 
 
 
@@ -279,12 +518,15 @@ app.post("/newyork", function(req,res){
 //     console.log("jajajaa");
 //     res.send("hahahaha");
 //  }));
+// delete function NewYork
     app.post("/newyork/delete", function (req,res){
         console.log("post nevr go here")
         console.log(req.body.idReview);
         const checkReviewId = req.body.checkbox;
      Place.findByIdAndRemove(checkReviewId, (err)=>{
-        if(!err) { res.redirect("/newyork")
+        if(!err) {
+            alert("***Sucessful delete your post***"); 
+            res.redirect("/newyork")
     } else {
         console.log("not found the ID review")
     }
@@ -292,6 +534,88 @@ app.post("/newyork", function(req,res){
        
         
     });
+
+ //delete function cairo
+ app.post("/cairo/delete", function (req,res){
+    console.log("post nevr go here")
+    console.log(req.body.idReview);
+    const checkReviewId = req.body.checkbox;
+ Place.findByIdAndRemove(checkReviewId, (err)=>{
+    if(!err) { 
+        alert("***Sucessful delete your post***");
+        res.redirect("/cairo")
+} else {
+    console.log("not found the ID review")
+}
+ })  
+   
+    
+});
+ //delete function paris
+ app.post("/paris/delete", function (req,res){
+    console.log("post nevr go here")
+    console.log(req.body.idReview);
+    const checkReviewId = req.body.checkbox;
+ Place.findByIdAndRemove(checkReviewId, (err)=>{
+    if(!err) { 
+        alert("***Sucessful delete your post***");
+        res.redirect("/paris");
+} else {
+    console.log("not found the ID review")
+}
+ })  
+   
+    
+});
+ //delete function rome
+ app.post("/rome/delete", function (req,res){
+    console.log("post nevr go here")
+    console.log(req.body.idReview);
+    const checkReviewId = req.body.checkbox;
+ Place.findByIdAndRemove(checkReviewId, (err)=>{
+    if(!err) {
+        alert("***Sucessful delete your post***");
+        res.redirect("/rome");
+} else {
+    console.log("not found the ID review")
+}
+ })  
+   
+    
+});
+ //delete function sydney
+ app.post("/sydney/delete", function (req,res){
+    console.log("post nevr go here")
+    console.log(req.body.idReview);
+    const checkReviewId = req.body.checkbox;
+ Place.findByIdAndRemove(checkReviewId, (err)=>{
+    if(!err) { 
+        alert("***Sucessful delete your post***");
+        res.redirect("/sydney")
+} else {
+    console.log("not found the ID review")
+}
+ })  
+   
+    
+});
+ //delete function tokyo
+ app.post("/tokyo/delete", function (req,res){
+    console.log("post nevr go here")
+    console.log(req.body.idReview);
+    const checkReviewId = req.body.checkbox;
+ Place.findByIdAndRemove(checkReviewId, (err)=>{
+    if(!err) { 
+        alert("***Sucessful delete your post***");
+        res.redirect("/tokyo")
+} else {
+    console.log("not found the ID review")
+}
+ })  
+   
+    
+});
+
 // create section for
 
   
